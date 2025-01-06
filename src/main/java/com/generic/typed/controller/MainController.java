a09dc195d7100edc310469e458fc82ebe26ea8a5
hye-inA <hye.ina0130@gmail.com>
2025-01-06 18:21:49 +0900

    feat : MainController 생성

---
+++ src/main/java/com/generic/typed/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.generic.typed.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "메인 스크린";
+    }
+}
