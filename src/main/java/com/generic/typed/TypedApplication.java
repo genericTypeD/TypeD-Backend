7b339987304c4c8734fad822905fdf71d77986a8
hye-inA <hye.ina0130@gmail.com>
2025-01-06 15:29:59 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/generic/typed/TypedApplication.java
@@ -0,0 +1,13 @@
+package com.generic.typed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypedApplication.class, args);
+    }
+
+}
