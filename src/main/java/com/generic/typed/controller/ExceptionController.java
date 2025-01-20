df8302fe47503b094fd2979b8f11161d00569b3f
hye-inA <hye.ina0130@gmail.com>
2025-01-20 13:24:48 +0900

    feat : 예외처리 handler(controller) 정의

---
+++ src/main/java/com/generic/typed/controller/ExceptionController.java
@@ -0,0 +1,48 @@
+package com.generic.typed.controller;
+
+import com.generic.typed.exception.TypedException;
+import com.generic.typed.response.ErrorResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ControllerAdvice
+public class ExceptionController {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
+        ErrorResponse response = ErrorResponse.builder()
+                .code("400")
+                .message("잘못된 요청입니다.")
+                .build();
+
+        // 필드명, 에러메세지
+        for (FieldError fieldError : e.getFieldErrors()) {
+            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return response;
+    }
+    @ResponseBody
+    @ExceptionHandler(TypedException.class)
+    public ResponseEntity<ErrorResponse> typedException(TypedException e) {
+        int statusCode = e.getStatusCode();
+
+        ErrorResponse body = ErrorResponse.builder()
+                .code(String.valueOf(statusCode))
+                .message(e.getMessage())
+                .build();
+
+        ResponseEntity<ErrorResponse> response = ResponseEntity.status(statusCode)
+                .body(body);
+
+        return response;
+    }
+}
